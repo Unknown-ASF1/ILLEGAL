5fcf8b17953482c50a06988cc6d7674952d2ea3d
Unknown-ASF1 <sapramudit9@gmail.com>
2026-08-06 01:36:27 +0530

    Some more changes

--- Backend/face_engine.py
+++ Backend/face_engine.py
@@ -1,10 +1,11 @@
 from insightface.app import FaceAnalysis
 
-# Smaller model (~4x less RAM than buffalo_l)
 app = FaceAnalysis(name="buffalo_s")
 
-# CPU mode
-app.prepare(ctx_id=-1)
+app.prepare(
+    ctx_id=-1,
+    det_size=(320,320)
+)
 
 def get_faces(image):
     return app.get(image)
